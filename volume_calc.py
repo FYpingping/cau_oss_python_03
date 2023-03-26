325b68a4423b0247e51067c86f168bc44887d711
FYpingping <71486591+FYpingping@users.noreply.github.com>
2023-03-26 17:22:42 +0900

    Update volume_calc.py
    restored first code

--- volume_calc.py
+++ volume_calc.py
@@ -2,15 +2,4 @@ length = float(input("가로의 길이:"))
 width = float(input("세로의 길이:"))
 height = float(input("높이의 길이:"))
 volume = length * width * height
-weight = float(input("택배의 무게:"))
-print("박스의 부피는",volume,"cm^3입니다.", sep=" ")
-
-if ((length > 120) or (width > 120) or (height > 120) or (weight > 25)):
-    fee = 12000
-elif ((length > 100) or (width > 100) or (height > 100) or (weight > 15)):
-    fee = 9000
-elif ((length > 80) or (width > 80) or (height > 80) or (weight > 5)):
-    fee = 6000
-else:
-    fee = 3000
-print("요금은",fee,"입니다.",sep=" ")
+print("박스의 부피는",volume,"입니다.")
