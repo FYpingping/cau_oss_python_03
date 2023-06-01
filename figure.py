4bd32daaaa9a5277abb899bc9edbc357b680c474
jung yonghee <jbh3059@gmail.com>
2023-06-01 20:34:57 +0900

    Add hw5

--- figure.py
+++ figure.py
@@ -4,30 +4,49 @@
 import math
 
 
-# 선에 대한 객체 생성 위한 클래스 정의
+
 class line:
-    __length = 0
-    
-    # 생성자 초기화 메소드. 값을 받아 __length 변수에 저장
-    def __init__(self, length):
-        self.__length = length
-    
-    # 인자로 받은 값을 __length 변수에 저장하는 메소드
-    def set_length(self, length):
-        self.__length = length
+    """
+    높이, 너비 객체 생성 위한 클래스 정의
+    외부에서 접근 불가능한 변수 width와 height을 선언했고
+    이를 통해 getter, setter 메소드를 정의
+    """
 
-    # __length 값을 반환받는 메소드
+    __width = 0
+    __height = 0
+    
+    # 생성자 초기화 메소드. 값을 받아 __height, __width 변수에 저장
+    # width는 가로의 길이, height는 세로의 길이
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+    
+    # 인자로 받은 값을 width, height 변수에 저장하는 메소드
+    def set_length(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    # 외부에서 width와 height 값을 알 수 있도록 반환해주는 메소드
     def get_length(self):
-        return self.__length
+        return self.__width, self.__height      #너비와 높이 값 반환
 
-# 인자로 받은 정사각형의 길이를 통해 정사각형의 넓이를 구하는 함수
-def area_square(length):
-    return length**2
+# 인자로 받은 너비와 높이 값을 통해 직사각형의 넓이를 구하는 함수
+def area_rectangle(width, height):
+    valueErrorReport(width, height)             #너비와 높이 값이 0이하면 ValueError에 대한 예외처리
+    return width*height                         # 직사각형의 넓이 int or float 값 반환
 
-# 인자로 받은 원의 반지름의 길이을 통해 원의 넓이를 구하는 함수
-def area_circle(length):
-    return length**2*math.pi
+# 인자로 받은 타원의 가로와 세로의 길이을 통해 타원의 넓이를 구하는 함수
+def area_eclipse(width, height):
+    valueErrorReport(width, height)             #가로와 세로 값이 0이하면 ValueError에 대한 예외처리
+    return width*height*math.pi                 #타원의 넓이 int or float값 반환
 
-# 인자로 받은 정삼각형의 한 변의 길이를 통해 정삼각형의 넓이를 구하는 함수
-def area_regular_triangle(length):
-    return length**2*math.sqrt(3)/4
+# 인자로 받은 직각삼각형의 가로와 세로의 길이를 통해 직각삼각형의 넓이를 구하는 함수
+def area_right_triangle(width, height):
+    valueErrorReport                            #가로와 세로 값이 0이하면 ValueError에 대한 예외처리
+    return width*height/2                       #직각삼각형의 넓이 int or float 값 반환
+
+# 인자로 받은 width와 height 값이 0이하라면 ValueError에 대한 예외처리를 하는 메소드
+def valueErrorReport(width, height):
+    if(width<=0 | height <= 0):
+        raise ValueError
+# %%
--- figure_main.py
+++ figure_main.py
@@ -1,23 +1,55 @@
-# %% figure 모듈을 import하여 작성한 프로그램
+# %%
 
+"""
+area_rectangle, area_right_triangle, area_ellipse 함수들에서
+예외 발생 시 0이하의 값은 입력할 수 없습니다를 출력하도록 변경!
+"""
 
-# 프로그램 실행에 필요한 모듈 import
 import figure
 
-# 모듈을 통해 10을 인자로 하는 생성자 호출
-myline = figure.line(10)
+#생성자를 통해 가로 10, 세로 20의 객체 myline을 생성
+myline = figure.line(10, 20)
+# 생성된 객체 myline의 가로, 세로 길이를 변수 width, height에 저장
+width, height = myline.get_length()
 
-# 모듈 속 길이 변수를 getter를 통해 구하고 그것으로 정사각형의 넓이를 구한다
-square = figure.area_square(myline.get_length())
-print(square)
+# implement exception handler
+try:
+    #figure 모듈의 직사각형의 넓이를 구하는 메소드를 통해 직사각형의 넓이를 rectangle 변수에 저장하고 출력
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    #width, height에 저장된 값이 0 이하인 예외가 발생하면 메세지를 출력
+    print("plese input positive number for width and height")
 
-# 모듈 속 길이 변수를 setter를 통해 값을 저장하고 새로 저장된 길이를 토대로 정삼각형의 넓이를 구한다
-myline.set_length(20)
-regular_triangle = figure.area_regular_triangle(myline.get_length())
-print(regular_triangle)
 
 
-# 모듈속 길이 변수를 setter를 통해 값을 저장하고 새로 저장된 길이를 토대로 원의 넓이를 구한다
-myline.set_length(30)
-circle = figure.area_circle(myline.get_length())
-print(circle)
+
+#객체에 저장된 가로, 세로 길이를 20, 30으로 변경
+myline.set_length(20, 30)
+#객체에 저장된 가로, 세로 길이를 변수 width, height에 저장
+width, height = myline.get_length()
+
+# implement exception handler
+try:
+    #figure 모듈의 직각삼각형의 넓이를 구하는 메소드를 통해 직각삼각형의 넓이를 triangle 변수에 저장하고 출력
+    triangle = figure.area_right_triangle(width, height)
+    print(triangle)
+except ValueError:
+    #width, height에 저장된 값이 0 이하인 예외가 발생하면 메세지를 출력
+    print("plese input positive number for width and height")
+
+
+
+#객체에 저장된 가로, 세로 길이를 30, 40으로 변경
+myline.set_length(30, 40)
+#객체에 저장된 가로, 세로 길이를 변수 width, height에 저장
+width, height = myline.get_length()
+
+# implement exception handler
+try:
+    #figure 모듈의 타원의 넓이를 구하는 메소드를 통해 타원의 넓이를 eclipse 변수에 저장하고 출력
+    eclipse = figure.area_eclipse(width, height)
+    print(eclipse)
+except ValueError:
+    #width, height에 저장된 값이 0이하인 예외가 발생하면 메세지를 출력
+    print("plese input positive number for width and height")
